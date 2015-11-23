8e4babab2e7e68c316c4d88156d97fee593476d9
Iablonskyi Ievgenii <eugen_yablonski@yahoo.com>
2015-11-23 16:37:52 +0700

    Initial Commit

---
+++ TweeterTagsTests/TweeterTagsTests.swift
@@ -0,0 +1,36 @@
+//
+//  TweeterTagsTests.swift
+//  TweeterTagsTests
+//
+//  Created by тигренок  on 11/23/15.
+//  Copyright (c) 2015 Midori.s. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TweeterTagsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
